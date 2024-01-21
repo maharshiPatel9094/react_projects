ea6e6486b06fc49b9afaf2b2a9ae277be0b7600c
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-01-21 18:39:07 -0500

    update in customeReact project
    update

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -10,7 +10,7 @@ function customRender(reactElement,mainContainer){
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
     for(let prop in reactElement.props){
-        if(prop == 'children') continue;
+        if(prop == 'children' ) continue;
         domElement.setAttribute(prop,reactElement.props[prop])
         mainContainer.appendChild(domElement) 
     }
