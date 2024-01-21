81a927db36ba273fae9074d5879e3bb36b6d2683
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-01-21 17:39:03 -0500

    update
    learned how to add an component in the two methods of creating an react project

---
+++ 01basicreact/src/helo.js
@@ -0,0 +1,8 @@
+// functions name should always start with the capital letters
+function Helo(){
+return(
+    <h1>Here is our component</h1>
+)
+}
+// for exporting the function
+export default Helo;
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import Helo from './helo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Helo/>
   </React.StrictMode>
 );
