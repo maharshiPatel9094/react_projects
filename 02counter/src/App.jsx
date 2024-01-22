80301a22961d6bda0ea7fe18e33963031505d1b1
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-01-22 12:51:17 -0500

    created project counter
    this project just update the value and decrease the value

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+// importing useState hook 
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+// this hook is responsible for changing the state
+// setCounter basically controls the counter
+  let [counter,setCounter] = useState(15)  
+  // function for adding the value
+  const addValue = () =>{
+   setCounter(counter+1)
+  }
+
+
+  const decreaseValue = () =>{
+    setCounter(counter-1)
+  }
+
+  return (
+    <>
+      <h1>React Project</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={decreaseValue}>Decrease Value</button>
+
+    </>
+  );
+}
+
+export default App;
