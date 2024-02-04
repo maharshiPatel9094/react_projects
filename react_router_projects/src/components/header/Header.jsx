09aaf8ef2e477506f9b985d84be9c94835e03179
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-02-04 13:53:43 -0500

    added user route

--- react_router_projects/src/components/header/Header.jsx
+++ react_router_projects/src/components/header/Header.jsx
@@ -59,6 +59,18 @@ export default function Header() {
                   About
                 </NavLink>
               </li>
+              <li>
+                {/* nav link for home */}
+                <NavLink
+                to="/contact"
+                // here we have added classes in backticks bcz when we change from home user should no that he changed
+                  className={({isActive}) =>
+                    `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  Contact
+                </NavLink>
+              </li>
             </ul>
           </div>
         </div>
--- react_router_projects/src/main.jsx
+++ react_router_projects/src/main.jsx
@@ -5,7 +5,9 @@ import './index.css'
 import Layout from './Layout.jsx'
 import Home from "./components/Home/Home.jsx"
 import About from "./components/About/About.jsx"
-import { createBrowserRouter } from 'react-router-dom'
+import Contact from "./components/Contact/Contact.jsx"
+import User from './components/User/User.jsx'
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import { RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
@@ -20,11 +22,29 @@ const router = createBrowserRouter([
       {
         path:"about",
         element: <About/>
+      },
+      {
+        path:"contact",
+        element: <Contact/>
+      },
+      {
+        path:"user/:userid",
+        element: <User/>
       }
+
     ] 
   }
 ])
 
+// const router = createBrowserRouter([
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout/>}>
+//       <Route path="" element={<Home/>}/>
+//       <Route path="/about" element={<About/>}/>
+//       <Route path="/contact" element={<Contact/>}/>
+//     </Route>
+//   )
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
