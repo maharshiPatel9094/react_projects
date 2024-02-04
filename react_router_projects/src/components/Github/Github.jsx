6cf0712554fb2d00f4a55cd5f1f4793a434df4fb
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-02-04 14:08:54 -0500

    added git hub image

--- react_router_projects/src/components/Github/Github.jsx
+++ react_router_projects/src/components/Github/Github.jsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function Github() {
-    const [data,setData] = useState([])
+  const [data, setData] = useState([]);
 
-    useEffect(()=>{
-fetch('https://api.github.com/users/maharshiPatel9094')
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    setData(data)
-})
-    },[])
-    return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4  text-3xl '>
-          Github Followers : {data.followers}
-        </div>
-    )
+  useEffect(() => {
+    fetch("https://api.github.com/users/maharshiPatel9094")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4  text-3xl ">
+      Github Followers : {data.followers}
+      <img src={data.avatar_url} alt="git picture" width={300} />
+    </div>
+  );
 }
 
-export default Github
+export default Github;
