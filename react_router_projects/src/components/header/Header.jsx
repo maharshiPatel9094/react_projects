f32a7b59c93efa2d807d909a52b457e689e8ac59
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-02-04 13:26:12 -0500

    updated about layout

--- react_router_projects/src/components/header/Header.jsx
+++ react_router_projects/src/components/header/Header.jsx
@@ -38,12 +38,25 @@ export default function Header() {
               <li>
                 {/* nav link for home */}
                 <NavLink
+                to="/"
                 // here we have added classes in backticks bcz when we change from home user should no that he changed
                   className={({isActive}) =>
                     `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                   }
                 >
                   Home
+                </NavLink>
+              </li>
+              <li>
+                {/* nav link for home */}
+                <NavLink
+                to="/about"
+                // here we have added classes in backticks bcz when we change from home user should no that he changed
+                  className={({isActive}) =>
+                    `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  About
                 </NavLink>
               </li>
             </ul>
