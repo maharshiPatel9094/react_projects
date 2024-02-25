0516a4ca5a42fba3001e97792403928fe123826d
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-02-25 17:49:28 -0500

    app.jsx updating

--- mega_blog/src/App.jsx
+++ mega_blog/src/App.jsx
@@ -1,12 +1,31 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-console.log(import.meta.env.REACT_APP_APPWRITE_URL);
-  return (
-    <>
-      <h1>Mega Blog</h1>
-    </>
-  )
+  // states
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h">
+
+    </div>
+  ) : (null)
 }
 
-export default App
+export default App;
---
+++ mega_blog/src/components/index.js
@@ -0,0 +1,9 @@
+// import will be done through this file 
+import Header  from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+
+export{
+    Header,
+    Footer
+}
